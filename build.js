d3556a3eed7f540ac9ada5674b1a88098b5f324f
Zack Anselm <zanselm5@gmail.com>
2016-09-06 23:58:02 +0000

    Nuked dev store

--- build.js
+++ build.js
@@ -9,44 +9,38 @@ const autoPrefix = require('postcss')([autoPrefixer]);
 
 /* Build Main App*/
 browserify("app-main/app.js")
-.transform(stringify, {
-      appliesTo: { includeExtensions: ['.hjs', '.html', '.whatever'] }
+    .transform(stringify, {
+        appliesTo: {
+            includeExtensions: ['.hjs', '.html', '.whatever']
+        }
     })
-.transform("babelify", {
+    .transform("babelify", {
         presets: ["es2015"]
     })
     .bundle()
     .pipe(fs.createWriteStream("dist/js/app.js"));
 
+/* Compile SCSS */
+let options = {
+    file: 'app-main/styles/app.scss',
+    outputStyle: 'compressed'
+};
 
-/* Build Store App*/
-browserify("app-store/app.jsx")
-  .transform("babelify", {presets: ["es2015", "react"]})
-  .bundle()
-  .pipe(fs.createWriteStream("dist/store/js/app.js"));
+sass.render(options, (err, result) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
 
+    autoPrefix.process(result.css.toString())
+        .then((result) => {
+            let dataString = result.css.toString();
+            let kbs = Buffer.byteLength(dataString) / 1000;
 
-  /* Compile SCSS */
-  let options = {
-      file: 'app-main/styles/app.scss',
-      outputStyle: 'compressed'
-  };
-
-  sass.render(options, (err, result) => {
-      if (err) {
-          console.log(err);
-          return;
-      }
-
-      autoPrefix.process(result.css.toString())
-      .then((result) => {
-          let dataString = result.css.toString();
-          let kbs = Buffer.byteLength(dataString) / 1000;
-
-          result.warnings().forEach(function (warn) {
-              console.warn(warn.toString());
-          });
-          fs.writeFileSync('dist/css/app.css', dataString, 'utf8');
-          fs.writeFileSync('dist/store/css/app.css', dataString, 'utf8');
-      });
-  });
+            result.warnings().forEach(function(warn) {
+                console.warn(warn.toString());
+            });
+            fs.writeFileSync('dist/css/app.css', dataString, 'utf8');
+            fs.writeFileSync('dist/store/css/app.css', dataString, 'utf8');
+        });
+});
