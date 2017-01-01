bec5d0f83eb91b572b1334cd665f780e57d5f8d7
Zack Anselm <zanselm5@gmail.com>
2017-01-01 14:46:26 -0500

    Styled login/register

--- src/components/pages/ArchivePage.js
+++ src/components/pages/ArchivePage.js
@@ -24,21 +24,6 @@ export default class ArchivePage extends React.Component {
 				<div className="row">
 					<div className="small-12 medium-8 large-9 columns">
 						<h1>Archive</h1>
-						<div className="row push-bottom">
-							<div className="small-12 medium-9 columns">
-								<div className="search-input">
-									<input type="search" placeholder="Enter search terms..." onChange={this.handleFilter}/>
-									<span className="fa fa-search search-icon"></span>
-								</div>
-		                    </div>
-							<div className="small-12 medium-3 columns">
-								<select id="orderParams" defaultValue="createdAt" onChange={this.handleSort}>
-		                            <option value="lastName">LP's</option>
-		                            <option value="email">EP's</option>
-		                            <option value="mobilePhone">Compilations</option>
-		                        </select>
-		                    </div>
-						</div>
 		                <h2>Under Construction</h2>
 		            </div>
 					<SideBar/>
